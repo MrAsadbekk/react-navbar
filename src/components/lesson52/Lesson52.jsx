35f73fe527e92ec81025cf6f0ed59bf063613e76
Qoryogdiyev <asadbekqoryogdiyev7@gmail.com>
2023-04-26 21:04:41 +0500

    fixed

--- src/components/lesson52/Lesson52.jsx
+++ src/components/lesson52/Lesson52.jsx
@@ -2,19 +2,19 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 const Lesson52 = () => {
-  const [data, setData] = useState([]);
+  //   const [data, setData] = useState([]);
 
-  const getData = async () => {
-    try {
-      const response = await axios.get("https://restcountries.com/v3.1/all");
-      setData(response.data);
-      console.log(data);
-    } catch (error) {}
-  };
+  //   const getData = async () => {
+  //     try {
+  //       const response = await axios.get("https://restcountries.com/v3.1/all");
+  //       setData(response.data);
+  //       console.log(data);
+  //     } catch (error) {}
+  //   };
 
-  useEffect(() => {
-    getData();
-  }, []);
+  //   useEffect(() => {
+  //     getData();
+  //   }, []);
 
   return <div></div>;
 };
