6817f542e460e0d2937a12602676e6cac67f66b1
Qoryogdiyev <asadbekqoryogdiyev7@gmail.com>
2023-04-26 10:35:53 +0500

    delete modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Navbar from "./components/navbar/Navbar";
-import Modal from "./components/modal/Modal";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Modal />
     </div>
   );
 }
