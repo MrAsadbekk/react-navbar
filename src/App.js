07a5d298db32b92693d2639979fc4e7d58eb9443
Qoryogdiyev <asadbekqoryogdiyev7@gmail.com>
2023-05-01 19:03:15 +0500

    lesson55

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import "./App.css";
-// import TestInput from "./components/testInput/TestInput";
-// import Data from "./components/data/Data";
-import Restcountries from "./components/Restcountries/Restcountries";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Error from "./components/error/Error";
 
 function App() {
   return (
     <div className="App">
-      {/* <TestInput /> */}
-      {/* <Data /> */}
       <BrowserRouter>
         <Routes>
-          <Route element={<Restcountries />} path="/" />
           <Route element={<Error />} path="error404" />
           <Route element={<Navigate to={"error404"} />} path="*" />
         </Routes>
